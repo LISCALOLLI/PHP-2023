840a30b28145c91f0ba929eeddc158fda3fd2ec9
Gabriel Riscarolli <112108495+LISCALOLLI@users.noreply.github.com>
2023-09-05 08:50:58 -0300

    Add files via upload

---
+++ contato.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+    <div class="container-left">
+    <h3>Telefone:</h3>
+    <h3>+55 (47) 989060383</h3>
+    
+    <h3>E-mail:</h3>
+    <a href="gabriel.riscarolli@unidavi.edu.br">gabriel.riscarolli@unidavi.edu.br</a>
+    
+    <h3>Mapa:</h3>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3548.2788356148594!2d-49.64441599999999!3d-27.21039409999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1693851627998!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="container-right">
+    <h3>Nome:</h3>
+    <input type="text" name="nome">
+    
+    <h3>Telefone:</h3>
+    <input type="number" name="tele">
+    
+    <h3>E-mail:</h3>
+    <input type="email" name="email2">
+    
+    <h3>Assunto:</h3>
+    <input type="text" name="ass">
+    
+    <h3>Mensagem:</h3>
+    <textarea name="msg" cols="30" rows="10"></textarea>
+    
+    <input type="submit" name="enviar" id="enviar" value="Enviar">
+    </div>
+</div>
+</body>
+</html>
